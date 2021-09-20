94a1780224d6788a7e651a82859c01b7829623fc
Mariano Mendez <marianito01@live.com>
2021-09-19 22:28:32 -0300

    Inicio de responsive design

--- src/contact.js
+++ src/contact.js
@@ -14,9 +14,8 @@ export default function displayContactHTML() {
         <h1 id="title">Contacto</h1>
 
         <div id='information'>
-            <p>
-            Nos encontramos en Palermo, el barrio más pintoresco de la ciudad de Buenos Aires.  Vení a conocer la experiencia JSbar. Vení a disfrutar el sabor del conocimiento!</p>
-            <h2>Te esperamos!</h2>
+            <p id="contacto">Nos encontramos en Palermo, el barrio más pintoresco de la ciudad de Buenos Aires, cerca del Rosedal de Palermo. Vení a conocer la experiencia JSbar. Vení a disfrutar el sabor del conocimiento!</p>
+            <h2 id="saludo">Te esperamos!</h2>
         </div>
     </div>
     `;
